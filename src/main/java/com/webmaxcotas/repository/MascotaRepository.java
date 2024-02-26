00f8ac6f77e8bddead813d4e5f05ee55d4d83ce6
DiegoNirva <diegonvz96@hotmail.com>
2024-02-26 19:20:09 -0300

    Se agrega los repositorios para las entidades Mascota, Vacuna y Veterinario

---
+++ src/main/java/com/webmaxcotas/repository/MascotaRepository.java
@@ -0,0 +1,24 @@
+package com.webmaxcotas.repository;
+
+import com.webmaxcotas.model.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MascotaRepository  extends JpaRepository<Mascota, Long> {
+
+    //usamos un Optional
+    Optional<Mascota> findByNombre(String nombre);
+
+
+    //query personalizada de JPQL
+    @Query(value = "SELECT m FROM Mascota m ORDER BY LOWER(m.nombres) ASC")
+    List<Mascota> findAllByOrderByNombreIgnorecaseAsc();
+
+
+
+}
--- src/main/java/com/webmaxcotas/repository/VacunaResository.java
+++ src/main/java/com/webmaxcotas/repository/VacunaResository.java
@@ -1,9 +1,9 @@
 package com.webmaxcotas.repository;
 
-import com.webmaxcotas.model.Veterinario;
+import com.webmaxcotas.model.Vacuna;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VeterinarioRepository extends JpaRepository<Veterinario, Long> {
+public interface VacunaResository extends JpaRepository<Vacuna, Long> {
 }
