5421bfff66edd094ebdb3ef3d6ca3225def99d4a
DiegoNirva <diegonvz96@hotmail.com>
2024-03-19 21:25:30 -0300

    se agregan mas controladores

---
+++ src/main/java/com/webmaxcotas/controller/AutoController.java
@@ -0,0 +1,40 @@
+package com.webmaxcotas.controller;
+
+import com.webmaxcotas.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.webmaxcotas.model.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AutoController {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @GetMapping("/")
+    public String redireccionarPaginaPrincipal(){
+    return "redirect:/mascotas";
+    }
+
+    @GetMapping("/login")
+    public String loginUsuario(){
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String SingUp(Model model){
+        model.addAttribute("usuario", new Usuario());
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String saveUsuario(Usuario usuario){
+        customUserDetailService.saveUsuario(usuario);
+        return "redirect:/login";
+    }
+
+}
